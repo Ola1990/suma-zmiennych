39bb71e6d17fec00b7d43895922b72fe673cfc5a
Ola Goźlińska <aleksandra.gozlinska@wp.pl>
2018-06-14 18:33:48 +0200

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-var liczbaPierwsza = 5;
-var liczbaDruga = 8;
-
 function suma(liczbaPierwsza, liczbaDruga) {
     var result = liczbaPierwsza + liczbaDruga;
     return result;
-    var value = suma(5, 8);
 }
+var value = suma(5, 8);
